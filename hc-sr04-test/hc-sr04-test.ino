de37618d1bcbe6180a29e48aaeb3b45e380cf536
Julian Dibrani <JulianDibr@gmail.com>
2020-05-10 20:22:09 +0200

    Basic LED output based on distance

--- hc-sr04-test/hc-sr04-test.ino
+++ hc-sr04-test/hc-sr04-test.ino
@@ -1,24 +1,42 @@
 const int echoPin = 34                                                                                                                                                                            ;
 const int trigPin = 27;
+
+const int redLed = 25;
+const int greenLed = 26;
+
 long duration;
 int distance;
 
 void setup() {
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
- Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
- digitalWrite(trigPin, LOW);
- delayMicroseconds(2);
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10);
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH);
- distance = duration * 0.03434 / 2;
- Serial.print("Distance: ");
- Serial.print(distance);
- Serial.println("cm");
- delay(300);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.03434 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+
+  if (distance < 2000) {
+    if (distance < 30) {
+      digitalWrite(redLed, LOW);
+      digitalWrite(greenLed, HIGH);
+    }
+    else {
+      digitalWrite(redLed, HIGH);
+      digitalWrite(greenLed, LOW);
+    }
+  }
+  delay(1000);
 }
